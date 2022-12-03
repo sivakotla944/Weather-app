6b3a62b5372a6bf3afea91b91c91871fc23b3df1
kotla madhu <kotlasiva944@gmail.com>
2022-12-03 16:59:43 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useSyncExternalStore } from "react";
+import cloud from "./assets/cloud.png"
 
 function App() {
+
+  const [search , setSearch] =  useState("");
+const [result , setResult]  = useState("");
+
+  const changeHandler = (e)=>{
+    setSearch(e.target.value)
+
+  }
+
+
+  const submitHandler = (e)=>{
+        e.preventDefault();
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+        response=> response.json()
+        ).then (
+        data=>{
+        const kelvin = data.main.temp;
+        const centigrade =  kelvin -273.15;
+        setResult ("Temperature at " + search  + "\n" +  Math.round(centigrade) + "°C")
+        setSearch("");
+
+  }
+).catch(error=>console.log(error))
+
+
+
+
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <div className='container'>
+        <div className=' card'>
+          <div className='card-body'>
+            <img src={cloud} alt="cloud image"/>
+            <h2>Weather app</h2>
+            <form onSubmit={submitHandler}>
+              <input className='searchbar' type="text" name="search" id="search" value={search}onChange={changeHandler}/><br></br>
+              
+              <input className='submit' type="submit"   value="Get temperature"/>
+            </form>
+
+            <div className='result'>
+              <h3>{result}</h3>
+            </div>
+
+
+          </div>
+        </div>
+        </div>
+      </center>
+
+      
+     
+ 
     </div>
   );
-}
+  }
 
 export default App;
